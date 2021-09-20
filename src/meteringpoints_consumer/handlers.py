32daf5a46b46662c4a58018d4658b63d5bcb87e9
rasmus godske <rasmusgodske@gmail.com>
2021-09-20 16:10:58 +0200

    Bug found

--- src/meteringpoints_consumer/handlers.py
+++ src/meteringpoints_consumer/handlers.py
@@ -24,7 +24,11 @@ def on_meteringpoint_update(
     meteringpoint.sector = msg.meteringpoint.sector
 
     if msg.meteringpoint.address:
-        controller.set_meteringpoint_address(msg.meteringpoint.address)
+        controller.set_meteringpoint_address(
+            session=session,
+            gsrn=msg.meteringpoint.gsrn,
+            address=msg.meteringpoint.address,
+        )
     if msg.meteringpoint.technology:
         controller.set_meteringpoint_technology(
             session=session,
