c3dc0cd9423fc93c2650aefd58e5e23495f5517c
Jakob Kristensen <jakob@jakobk.dk>
2021-09-28 16:43:47 +0200

    Fixed broken entrypoint scripts

--- src/entrypoint_api.sh
+++ src/entrypoint_api.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd src/migrations && alembic upgrade head && cd ../..
+set -e
+
+cd migrations && alembic upgrade head && cd ..
 gunicorn 'meteringpoints_api.app:create_app()' -w 2 --threads 2 -b 0.0.0.0:80
