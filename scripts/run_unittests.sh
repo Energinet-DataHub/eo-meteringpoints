53c085fbfdbb2a3536a055e74afb83a755ca8057
rasmus godske <rasmusgodske@gmail.com>
2021-10-20 16:08:25 +0200

    added test action

---
+++ scripts/run_unittests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if ! docker info > /dev/null 2>&1; then
+  echo "This script uses docker, and it isn't running - please start docker and try again"
+  exit 1
+fi
+
+export PYTHONPATH=$(pwd)/src
+pytest tests/unittest
