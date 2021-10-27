8747729dc6f28d53681bb4e6da7b3c889c4454f9
Jakob Kristensen <jakob@jakobk.dk>
2021-10-27 15:22:08 +0200

    Updated workflow

--- scripts/run_unittests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ! docker info > /dev/null 2>&1; then
-  echo "This script uses docker, and it isn't running - please start docker and try again"
-  exit 1
-fi
-
-export PYTHONPATH=$(pwd)/src
-pytest tests/unittest
