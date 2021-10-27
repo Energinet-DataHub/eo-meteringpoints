3140e792ce63df17b194b1d7a006c0733b5c5893
Jakob Kristensen <jakob@jakobk.dk>
2021-10-27 15:11:13 +0200

    Updated workflow

--- src/meteringpoints_shared/config.py
+++ src/meteringpoints_shared/config.py
@@ -4,13 +4,13 @@ import os
 # -- General -----------------------------------------------------------------
 
 # Secret used to sign internal token
-INTERNAL_TOKEN_SECRET = os.environ['INTERNAL_TOKEN_SECRET']
+INTERNAL_TOKEN_SECRET = os.environ.get('INTERNAL_TOKEN_SECRET', '')
 
 
 # -- Message Bus -------------------------------------------------------------
 
 # Message Bus host
-MESSAGE_BUS_HOST = os.environ['MESSAGE_BUS_HOST']
+MESSAGE_BUS_HOST = os.environ.get('MESSAGE_BUS_HOST', '')
 
 # Message Bus post
 MESSAGE_BUS_PORT = int(os.environ.get('MESSAGE_BUS_PORT', 9092))
@@ -22,7 +22,7 @@ MESSAGE_BUS_SERVERS = [f'{MESSAGE_BUS_HOST}:{MESSAGE_BUS_PORT}']
 # -- SQL ---------------------------------------------------------------------
 
 # SqlAlchemy connection string
-SQL_URI = os.environ['SQL_URI']
+SQL_URI = os.environ.get('SQL_URI', '')
 
 # Number of concurrent connection to SQL database
 SQL_POOL_SIZE = int(os.getenv('SQL_POOL_SIZE', 1))
